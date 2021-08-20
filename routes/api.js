96fb6040be369e0862435df58d62cde55517df9e
muzhaxyz <89118243+muzhaxyz@users.noreply.github.com>
2021-08-20 16:41:36 +0900

    Update

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "alpin"
+var creator = "muzha"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["alpin1", "alpin1"];
+const listkey = ["zeanxyz", "zeanxyzz"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
